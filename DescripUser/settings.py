a77381ac0d09d07922b47daea37aa2125d8d7fae
Aguilera2509 <ja846699@gmail.com>
2022-07-04 10:27:35 -0400

    heroku settings

--- DescripUser/settings.py
+++ DescripUser/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,11 @@ WSGI_APPLICATION = 'DescripUser.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'), 
+        conn_max_age=30,
+        ssl_require=True
+    )
 }
 
 
