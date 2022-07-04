e44d25d4daa1d83f04406f42ed6834e3525ffa17
Aguilera2509 <ja846699@gmail.com>
2022-07-04 09:42:26 -0400

    BBDD empty to heroku

--- DescripUser/settings.py
+++ DescripUser/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-#SECRET_KEY = ''
+#SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-2hkdw(bjdp4z(3v@r6#-@57)@(((ix0n#h5560cmtftzzx41si'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
